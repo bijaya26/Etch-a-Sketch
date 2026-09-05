e7e9c757ffa2bfa41e3996db25fd5077c9d03f27
Bijaya <bijaypokharel78@gmail.com>
2026-09-05 15:18:03 +0100

    64 BY 64 Div create

---
+++ script.js
@@ -0,0 +1,41 @@
+const body= document.querySelector("body");
+const div=document.createElement("div");
+const button=document.querySelector("#button");
+const input=document.querySelector("#input");
+div.style.border="2px solid black";
+div.style.display="flex";
+div.style.width="50%"
+div.style.aspectRatio="1";
+
+div.style.flexDirection="row";
+div.style.flexWrap="wrap";
+body.appendChild(div);
+
+
+function divCreate(number){
+    for(let i=1; i<=number*number; i++){
+        const insideDiv=document.createElement("div");
+        
+    
+        insideDiv.style.width=(100/number)+"%";
+        insideDiv.style.aspectRatio="1";
+        insideDiv.style.border="1px solid black";
+        insideDiv.style.boxSizing="border-box";
+        div.appendChild(insideDiv);
+        insideDiv.addEventListener("mouseenter",(event)=>{
+            insideDiv.style.backgroundColor="orange";
+        })
+       
+    }
+    
+     
+}
+
+
+function getButtonClicked(){
+    button.addEventListener("click",()=>{
+        const number= input.value;
+        divCreate(number)
+        input.value="";})};
+
+getButtonClicked();
